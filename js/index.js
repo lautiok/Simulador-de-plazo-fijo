83ee4dc398eda1830aad58c5450a8ed1acac77c2
Nahuel <nahuelguerra56b@gmail.com>
2023-06-30 19:01:19 -0300

    es6

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 function formatInput(element) {
-    var valor = element.value;
+    const valor = element.value;
   
     // Remover cualquier separador de miles existente (puntos o comas)
-    var valorSinSeparador = valor.replace(/[.,]/g, '');
+    const valorSinSeparador = valor.replace(/[.,]/g, '');
   
     // Agregar el separador de miles solo si es el primer input
     if (element.id === "monto") {
-      var valorConSeparador = valorSinSeparador.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      const valorConSeparador = valorSinSeparador.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
       element.value = valorConSeparador;
     } else {
       element.value = valorSinSeparador;
     }
 }
 function calcularPlazoFijo() {
-    var monto = parseFloat(document.getElementById("monto").value.replace(/[.,]/g, ''));
-    var porcentajeAnual = parseFloat(document.getElementById("porcentaje").value);
+    const monto = parseFloat(document.getElementById("monto").value.replace(/[.,]/g, ''));
+    const porcentajeAnual = parseFloat(document.getElementById("porcentaje").value);
   
-    var porcentajeMensual = porcentajeAnual / 12 / 100;
-    var montoMensual = monto * porcentajeMensual;
-    var gananciaTotal = montoMensual;
+    const porcentajeMensual = porcentajeAnual / 12 / 100;
+    const montoMensual = monto * porcentajeMensual;
+    const gananciaTotal = montoMensual;
   
-    var gananciaFormateada = gananciaTotal >= 1000 ? gananciaTotal.toLocaleString() : gananciaTotal.toFixed(2);
+    const gananciaFormateada = gananciaTotal >= 1000 ? gananciaTotal.toLocaleString() : gananciaTotal.toFixed(2);
   
     document.getElementById("ganancia").innerHTML = "La ganancia generada mensualmente es: $" + gananciaFormateada;
 }
